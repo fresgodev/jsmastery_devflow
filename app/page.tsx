e1fca05c23a3deb07924cb5846d1df7872376b05
Frank Estrada Gonzalez <frankestradagonzalez@gmail.com>
2024-11-07 00:28:13 -0700

    fonts setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1 className="h1-bold">Welcome to the world of nextjs</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of nextjs</h1>
+      <h1 className="h1-bold font-inter">
+        Welcome to the world of nextjs (inter)
+      </h1>
+      <h1 className="h1-bold font-space">
+        Welcome to the world of nextjs (inter)
+      </h1>
+    </>
+  );
 }
