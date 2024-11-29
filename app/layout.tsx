fb8cfeb9c0a1ac000e813ad65585a255720bacb7
Frank Estrada Gonzalez <frankestradagonzalez@gmail.com>
2024-11-28 23:47:21 -0700

    added auth routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import React from "react";
 
 import "./globals.css";
-import Navbar from "@/components/navigation/navbar";
 import ThemeProvider from "@/context/theme";
 
 const inter = localFont({
@@ -42,7 +41,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
